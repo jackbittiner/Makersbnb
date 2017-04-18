00c9225adcdb3576e15089962f8b34fd4d46ed2b
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-18 17:41:50 +0100

    fixed references to spaces from controller

---
+++ app/controllers/users.rb
@@ -0,0 +1,23 @@
+class MakersBnB < Sinatra::Base
+
+
+  get '/' do
+    @user = User.new
+    erb :'users/index'
+  end
+
+  post '/users' do
+    @user = User.create(email: params[:email],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation],
+    name: params[:name])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/spaces')
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      erb :'users/index'
+    end
+  end
+
+end
