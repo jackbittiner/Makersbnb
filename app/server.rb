ded2004816e832777eb7498b7e8d586c5433193e
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-18 17:41:50 +0100

    fixed references to spaces from controller

--- app/server.rb
+++ app/server.rb
@@ -9,9 +9,9 @@ class MakersBnB < Sinatra::Base
 
   enable :partial_underscores
 
-  # helpers do
-  #   def current_user
-  #     @current_user ||= User.get(session[:user_id])
-  #   end
-  # end
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
 end
