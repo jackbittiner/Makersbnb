23bd7c38fc14f59f3845b2fd20c492296f39f1f0
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-19 09:21:40 +0100

    added user sign in

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -10,3 +10,11 @@ def sign_up(name: "Jack Dopeyman",
     fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
+
+def sign_in(email: "jackdopeyman@hotmail.com",
+              password: "stephstephsteph")
+  visit '/sessions/new'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  click_button 'Sign in'
+end
