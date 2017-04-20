cd35c3235a44a00650a541ed44d8580fe988ce3b
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-20 12:53:54 +0100

    fix broken tests due to routing

--- spec/features/user_sign_out_spec.rb
+++ spec/features/user_sign_out_spec.rb
@@ -12,7 +12,7 @@ feature 'User signs out' do
   scenario 'while being signed in' do
     sign_in(email: 'test@test.com', password: 'test')
     click_button 'Sign out'
-    expect(current_path).to eq('/')
+    expect(current_path).to eq('/users/new')
     expect(page).to have_content('goodbye!')
     expect(page).not_to have_content('Welcome, test@test.com')
   end
