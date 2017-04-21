1646636fac31457d0c744afdefa586c701a1d3dc
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-21 15:01:26 +0100

    WIP update my space

--- spec/features/update_my_space_spec.rb
+++ spec/features/update_my_space_spec.rb
@@ -7,6 +7,26 @@ feature 'Ability to update an existing space' do
     visit '/spaces/user/Jack'
     click_link 'Casa de Rodriguez'
 
-    expect(page).to have_selector(:link_or_button, 'Update space')
+    expect(page).to have_selector(:link_or_button, 'Update my space')
   end
+
+  scenario 'A updated space should be different after update' do
+    sign_up
+    create_listing
+
+    visit '/spaces/user/Jack'
+    click_link 'Casa de Rodriguez'
+
+    click_button 'Update my space'
+    expect(current_path).to eq '/spaces/update'
+
+    fill_in 'name', with: 'Jacks House'
+    click_button 'Update my space'
+
+    expect(current_path).to eq '/spaces/user/Jack'
+    expect(page).not_to have_selector(:link_or_button, 'Casa de Rodriguez')
+    expect(page).to have_selector(:link_or_button, 'Jacks House')
+
+  end
+
 end
