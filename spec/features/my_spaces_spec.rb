ac578d70ec7d382729818f968c37d7e55da407b7
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-21 11:46:29 +0100

    allow user to navigate to own spaces & make logo a link to homepage

--- spec/features/my_spaces_spec.rb
+++ spec/features/my_spaces_spec.rb
@@ -4,14 +4,28 @@ feature "viewing a user's spaces" do
 
     sign_up
     create_listing(name: 'Casa de Jack',
-                      description: 'Has a functioning water closet',
-                      price: '1000',
-                      available_from: '18/04/17',
-                      available_to: '18/04/18')
+                  description: 'Has a functioning water closet',
+                  price: '1000',
+                  available_from: '18/04/17',
+                  available_to: '18/04/18')
     visit '/spaces/user/Jack'
     expect(page).to have_content("Casa de Jack")
     expect(page).to have_content("Jack's spaces")
+  end
 
+  scenario 'I can navigate to my listed spaces' do
+    sign_up
+    create_listing
+    create_listing(name: 'Casa de Jack',
+                  description: 'Has a functioning water closet',
+                  price: '1000',
+                  available_from: '18/04/17',
+                  available_to: '18/04/18')
+    visit '/'
+    expect(page).to have_selector(:link_or_button, 'My Spaces')
+
+    click_link 'My Spaces'
+    expect(current_path).to eq '/spaces/user/Jack'
   end
 
 end
